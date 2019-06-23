a1823d173097985ca0a96a6cfee344c2054b9880
keymoon <kymn0116@gmail.com>
2019-06-23 11:31:27 +0900

    [add] webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+﻿const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: path.resolve(__dirname, 'src/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        library: 'sidemenu'
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'src')]
+    },
+    externals: {
+        jquery: 'jQuery'
+    },
+    optimization: {
+        minimize: false
+    }
+};
+
